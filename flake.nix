08f34602eb8b02d07e4f4490532c2cd7f21b77b1
patryk3211 <patrykmierzy@gmail.com>
2023-08-19 12:24:39 +0200

    Updated flake inputs

--- flake.nix
+++ flake.nix
@@ -128,7 +128,8 @@
     };
 
     nil = {
-      url = "github:oxalica/nil?rev=18de045d7788df2343aec58df7b85c10d1f5d5dd";
+      # url = "github:oxalica/nil?rev=18de045d7788df2343aec58df7b85c10d1f5d5dd";
+      url = "github:oxalica/nil";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
