03efe0faf4e931be1fe21ea10443ffb7a393f9f8
patryk3211 <patrykmierzy@gmail.com>
2023-09-06 16:44:03 +0200

    Variable tab size based on file type definitions

--- modules/basic.nix
+++ modules/basic.nix
@@ -35,6 +35,16 @@ in {
       set tabstop=${toString cfg.tabWidth}
       set shiftwidth=${toString cfg.tabWidth}
       set softtabstop=${toString cfg.tabWidth}
+
+      let g:default_tab_size = 4
+      let g:tab_sizes = { }
+      function s:set_shift_width(filetype)
+          let l:size = get(g:tab_sizes, a:filetype, g:default_tab_size)
+          exec 'set shiftwidth='.l:size
+          exec 'set tabstop='.l:size
+          exec 'set softtabstop='.l:size
+      endfunction
+      autocmd FileType * call s:set_shift_width(expand('<amatch>'))
     '';
   };
 }
